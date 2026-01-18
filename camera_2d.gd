944710d0ac4a64ae2fb5a4078f085acb4d1b249f
ItsKareem <1022027.kareem@stemegypt.edu.eg>
2026-01-18 03:38:01 +0200

    Basic Main Menu and Demo

--- camera_2d.gd
+++ camera_2d.gd
@@ -2,9 +2,9 @@ extends Camera2D
 
 @export var tilemap: TileMap
 
-func _ready() -> void:
-	var mapRect = tilemap.get_used_rect()
-	var tileSize = tilemap.cell_quadrant_size
-	var worldSizeInPixels = mapRect.size * tileSize
-	limit_right = worldSizeInPixels.x -16
-	limit_bottom = worldSizeInPixels.y -16
+#func _ready() -> void:
+#	var mapRect = tilemap.get_used_rect()
+#	var tileSize = tilemap.cell_quadrant_size
+#	var worldSizeInPixels = mapRect.size * tileSize
+#	limit_right = worldSizeInPixels.x -16
+#	limit_bottom = worldSizeInPixels.y -16
