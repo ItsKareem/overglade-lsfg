d9992f61c2ecad767b833e88256b0a4b8be7c58b
ItsKareem <1022027.kareem@stemegypt.edu.eg>
2026-01-21 14:54:49 +0200

    Camera Update

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,6 +1,51 @@
 extends Camera2D
 
 @export var tilemap: TileMap
+@onready var player = $"../Player"
+
+const SCREEN_W := 256
+const SCREEN_H := 176
+
+var last_room : Vector2
+var forced_limits := false
+
+func update_camera_limits():
+	if forced_limits:
+		return
+
+	var room := Vector2(
+		floor(player.global_position.x / SCREEN_W),
+		floor(player.global_position.y / SCREEN_H)
+	)
+
+	var left   := room.x * SCREEN_W
+	var top    := room.y * SCREEN_H
+	var right  := left + SCREEN_W
+	var bottom := top + SCREEN_H
+
+	limit_left   = left
+	limit_top    = top
+	limit_right  = right
+	limit_bottom = bottom
+
+func _process(delta):
+	var current_room = Vector2(
+		floor(player.global_position.x / SCREEN_W),
+		floor(player.global_position.y / SCREEN_H)
+	)
+
+	if current_room != last_room:
+		last_room = current_room
+		update_camera_limits()
+		snap_to_room()
+
+func snap_to_room():
+	global_position = Vector2(
+		limit_left + SCREEN_W / 2,
+		limit_top + SCREEN_H / 2
+	)
+
+
 #@export var smoothingVar = 5.0
 
 #var actual_cam_pos : Vector2
